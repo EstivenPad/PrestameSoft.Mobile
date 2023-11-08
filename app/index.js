92944910c149ef599911e5685cf751620f547dc7
EstivenPad <estivenps26@gmail.com>
2023-11-08 09:31:56 -0400

    Initializing application

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import { Stack } from 'expo-router';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function Page() {
+    return (
+        // <SafeAreaView>
+        //     <Stack.Screen options={{
+        //         headerTitle: "HEADER PRESTAMOS",
+        //         headerStyle: { backgroundColor: '#f4511e' },
+        //         headerTintColor: '#fff'
+        //     }}>
+        //     </Stack.Screen>
+            
+        // </SafeAreaView>
+            <Text>Home Page</Text>
+    );
+}
